2a1820c6fefc0d0b2b0cae65b88763b57fbb3502
Jitender Gulia <guliajitender@yahoo.com>
2015-05-27 20:08:06 -0400

    First Commit for New Solution

---
+++ ActivityCenter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActivityCenter.MvcApplication" Language="C#" %>
---
+++ ActivityCenter/Models/Home/HomeViewDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActivityCenter.Models.Home
+{
+    public class HomeViewDataModel
+    {
+        public string Label1 { get; set; }
+        public string Label2 { get; set; }
+    }
+}
